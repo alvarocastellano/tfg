e5c446f3a6fc18957a177d02d5704cfa7de2e0aa
alvarocastellano <91947029+alvarocastellano@users.noreply.github.com>
2024-10-08 13:54:00 +0200

    Add: Creación de la estructura base del proyecto

---
+++ worldin/worldin/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from main import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
+    #path('login/', views.user_login, name='login'),
+    #path('register/', views.register, name='register'),
+    #path('world/', views.world_page, name='world'),
+    #path('profile/', views.profile, name='profile'),
+    #path('profile/edit/', views.edit_profile, name='edit_profile'),
+    #path('logout/', views.logout_view, name='logout'),
+
+
+
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
