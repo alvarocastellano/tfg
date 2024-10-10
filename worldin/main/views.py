15db91993b5586bc31c4bfe52d2d04082bf19ad5
alvarocastellano <91947029+alvarocastellano@users.noreply.github.com>
2024-10-10 12:54:20 +0200

    Add: Pagina de contacto

--- worldin/main/views.py
+++ worldin/main/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import  render
 def home(request):
     return render(request, 'home.html')
 
+def contact_us(request):
+    return render(request, 'contact_us.html')
--- worldin/worldin/urls.py
+++ worldin/worldin/urls.py
@@ -7,6 +7,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('contact_us/', views.contact_us, name='contact_us'),
     path('admin/', admin.site.urls),
     #path('login/', views.user_login, name='login'),
     #path('register/', views.register, name='register'),
